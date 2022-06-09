914c2a4656296cd02b8a52af2ae90b33a5014d25
Jacob Zimmer <jacobzimmer1@outlook.com>
2022-06-08 21:41:34 -0600

    Added code

--- for.js
+++ for.js
@@ -1,22 +1,34 @@
 // As you complete each challenge, RUN THE CODE to ensure it works. You can do this by
 // running `node <filepath>`. If you are in this directory, you'd run `node for.js`.
-  
+
 // Challenge 1
 // Use a for statement to log to the console all even numbers between 25 and 75.
+var numbers;
+for(i = 26; i >75; i+=2 ) {
+  console.log(i);
+}
 
 
 
 // Challenge 2
 // Use a for statement to log to the console a complete sentence stating the weather using the Array below:
 weather_options = ["sunny", "snowy", "freezing", "partly cloudy", "drizzly"]
-
+for (var i=0; i < weather_options.length; i++){
+  console.log("The weather is" + weather_options[i]);
+}
 
 // Challenge 3
 // Use a for statement to log to the console only the words that are exactly 3 characters in length
 words = ["car", "bike", "bus", "van", "scooter", "truck", "rv"]
+for(var i = 0; i < words.length; i++){
+  let newwords = words[i].split(',');
 
-// 📹 Record yourself live-coding and talking through any ONE of the challenges. 
-  // The recording doesn't have to be the first time you are solving the problem. 
-  // Use technical vocabulary and explain each part to demonstrate your understanding! 
+  for (j = 0; newwords.length; j++){
+    console.log(newwords[j].split(','));
+  }
+}
+
+// 📹 Record yourself live-coding and talking through any ONE of the challenges.
+  // The recording doesn't have to be the first time you are solving the problem.
+  // Use technical vocabulary and explain each part to demonstrate your understanding!
   // Share the link to your video in your small group channel.
-
